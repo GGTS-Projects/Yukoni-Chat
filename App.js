546a45da65244c87d4d32f1562eb30b462c48a05
Umar Abbas <Umar Abbas>
2017-03-22 06:21:21 +0500

    folder change

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text>hello! i m umar.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
