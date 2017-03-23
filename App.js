19696fd1eae15611c7f6864ebd5a2b9443a9b82e
umar.abbas <omer.ithj@gmail.com>
2017-03-23 21:19:21 +0500

    App Setup

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/App';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>hello! i m umar.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+           <Main/>
     );
   }
 }
