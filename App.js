9c50fa9ed46eb2c5789a183ec16bb287944583fd
Umar Abbas <Umar Abbas>
2017-03-25 07:12:15 +0500

    Font for expo

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import App from './src/App.js';
+import Ap from './src/App.js';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    Font.loadAsync({
+      'Rubik-Regular': require('./assets/fonts/Rubik-Regular.ttf'),
+    });
+  }
+
   render() {
     return (
-        <App />
+        <Ap />
     );
   }
 }
